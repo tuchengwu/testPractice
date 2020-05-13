9b377ec00497184f8860a12dbd0a12268ce7b2f9
tuchengwu <tuchengwu19991218@qq.com>
2020-05-13 11:17:36 +0800

    添加部分等价类测试用例、修复闰年判断

--- src/main/java/calendar/CaculateNextDay.java
+++ src/main/java/calendar/CaculateNextDay.java
@@ -70,7 +70,7 @@ public class CaculateNextDay {
             case 12:
                 return 31;
             case 2:
-                if(year%4==0) {
+                if(year%400==0 || ( year%4==0 && year%100!=0 )) {
                     return 29;
                 } else {
                     return 28;
